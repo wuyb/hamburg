494c70e01cb4b0ab3036fe24e95b3b29f0ef9a9e
Yanbo Wu <yanbo.wu@gmail.com>
2012-08-21 08:57:34 +0800

    add date information to tooltip

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,58 @@
 //= require jquery_ujs
 //= require_tree .
 //= require jquery.ui.all
+    function formatDate(d, fmt) {
+        if (typeof d.strftime == "function") {
+            return d.strftime(fmt);
+        }
+        var leftPad = function(n, pad) {
+            n = "" + n;
+            pad = "" + (pad == null ? "0" : pad);
+            return n.length == 1 ? pad + n : n;
+        };
+        
+        var r = [];
+        var escape = false;
+        var hours = d.getHours();
+        var isAM = hours < 12;
+
+        var hours12;
+        if (hours > 12) {
+            hours12 = hours - 12;
+        } else if (hours == 0) {
+            hours12 = 12;
+        } else {
+            hours12 = hours;
+        }
+
+        for (var i = 0; i < fmt.length; ++i) {
+            var c = fmt.charAt(i);
+            
+            if (escape) {
+                switch (c) {
+                case 'd': c = leftPad(d.getDate()); break;
+                case 'e': c = leftPad(d.getDate(), " "); break;
+                case 'H': c = leftPad(hours); break;
+                case 'I': c = leftPad(hours12); break;
+                case 'l': c = leftPad(hours12, " "); break;
+                case 'm': c = leftPad(d.getMonth() + 1); break;
+                case 'M': c = leftPad(d.getMinutes()); break;
+                case 'S': c = leftPad(d.getSeconds()); break;
+                case 'y': c = leftPad(d.getFullYear() % 100); break;
+                case 'Y': c = "" + d.getFullYear(); break;
+                case 'p': c = (isAM) ? ("" + "am") : ("" + "pm"); break;
+                case 'P': c = (isAM) ? ("" + "AM") : ("" + "PM"); break;
+                case 'w': c = "" + d.getDay(); break;
+                }
+                r.push(c);
+                escape = false;
+            }
+            else {
+                if (c == "%")
+                    escape = true;
+                else
+                    r.push(c);
+            }
+        }
+        return r.join("");
+    }
