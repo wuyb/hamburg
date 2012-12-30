c360a28c9ddc5b7885d63f2b5623507b27408f6d
Yanbo Wu <yanbo.wu@gmail.com>
2012-12-31 06:29:35 +0800

    test deploy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery.ui.all
 //= require jquery
+//= require jquery.ui.all
+//= require twitter/bootstrap
 //= require flot/jquery.flot
 //= require jquery_ujs
 //= require_tree .
